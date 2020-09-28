f84a06dc7811dedb095bff873a08a6d19cd04bcb
Brian Kennedy <bpkennedy@gmail.com>
2020-09-27 22:25:46 -0500

    Add Github Action to run lint and e2e tests on push

--- test/cypress/util.js
+++ test/cypress/util.js
@@ -1,5 +1,10 @@
 export function mockAlgolia () {
-  const queries = `https://tf6ehqug2k-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (3.35.1); Browser (lite); JS Helper (2.28.1); vue-instantsearch 1.7.0&x-algolia-application-id=${Cypress.env('ALGOLIA_APP_ID')}&x-algolia-api-key=${Cypress.env('ALGOLIA_KEY')}`
+  let queries = ''
+  if (process.env.CI === true) {
+    queries = `https://tf6ehqug2k-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (3.35.1); Browser (lite); JS Helper (2.28.1); vue-instantsearch 1.7.0&x-algolia-application-id=${process.env.CYPRESS_ALGOLIA_APP_ID}&x-algolia-api-key=${process.env.CYPRESS_ALGOLIA_KEY}`
+  } else {
+    queries = `https://tf6ehqug2k-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (3.35.1); Browser (lite); JS Helper (2.28.1); vue-instantsearch 1.7.0&x-algolia-application-id=${Cypress.env('ALGOLIA_APP_ID')}&x-algolia-api-key=${Cypress.env('ALGOLIA_KEY')}`
+  }
   cy.fixture('initialAlgoliaResults').as('results')
   cy.server()
   cy.route('POST', queries, '@results')

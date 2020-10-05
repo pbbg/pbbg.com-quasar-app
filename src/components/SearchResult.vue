267363ce37377e2dad3835fcff13c055fae19310
Brian Kennedy <bpkennedy@gmail.com>
2020-10-04 23:25:04 -0500

    Establish some app-wide css defaults based on current pbbg.com

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -7,8 +7,8 @@
     <q-card-section class="row flex">
       <q-avatar
         size="3rem"
-        color="primary"
-        class="rating-badge"
+        color="accent"
+        class="rating-badge text-secondary"
       >
         {{ result.rating }}
       </q-avatar>
@@ -20,7 +20,7 @@
         <div class="text-h5 q-mt-sm q-mb-xs">
           {{ result.name }}
         </div>
-        <div class="text-grey">
+        <div class="text-secondary">
           {{ result.short_description }}
         </div>
       </q-card-section>
@@ -29,10 +29,10 @@
     <q-separator />
 
     <q-card-section>
-      <div class="text-caption text-grey">
+      <div class="text-caption text-secondary">
         Latest review: {{ result.latest_review_rating }}/5 by <a :href="result.latest_review_user_path">{{ result.latest_review_name }}</a> on {{ result.latest_review_created_at }}
       </div>
-      <p class="text-caption text-grey-6 q-ma-none">
+      <p class="text-caption text-secondary q-ma-none">
         {{ result.latest_review_body }}
       </p>
     </q-card-section>

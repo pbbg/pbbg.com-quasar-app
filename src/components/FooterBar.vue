f05df50baa915a7171397e30ba2108398afca3ef
Brian Kennedy <bpkennedy@gmail.com>
2020-10-31 15:00:05 -0500

    Simplify footer, update text, display version and changelog button

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,21 +1,56 @@
+<script>
+import { version } from '../../package.json'
+
+export default {
+  data() {
+    return {
+      version,
+    }
+  },
+}
+</script>
 <template>
   <q-footer
     bordered
     dense
-    class="bg-grey-1 text-grey-6"
+    class="row bg-grey-1 q-pa-md text-grey-6"
+    :class="{'column': $q.screen.lt.sm}"
   >
-    <q-toolbar>
-      <q-toolbar-title class="text-body2 text-center">
-        by
-        <a
-          href="https://foohonpie.com/"
-          target="_blank"
-        >Foohon Pie LLC</a>
-        with help from <a
-          href="https://pbbg.com/humans.txt"
-          target="_blank"
-        >friends and supporters</a>
-      </q-toolbar-title>
-    </q-toolbar>
+    <div class="col self-center text-body2 text-center">
+      by
+      <a
+        href="https://foohonpie.com/"
+        target="_blank"
+      >Foohon Pie LLC</a>
+      with help from <a
+        href="https://discord.gg/KvwaEqC"
+        target="_blank"
+      >friends</a>, <a
+        href="https://www.patreon.com/foohonpie"
+        target="_blank"
+      >supporters</a>, and <a
+        href="https://github.com/pbbg"
+        target="_blank"
+      >contributors</a>.
+    </div>
+    <q-btn
+      no-caps
+      outline
+      color="secondary"
+      :label="'v' + version"
+      aria-label="Changelog Button"
+      :to="{ path: '/changelog' }"
+      class="col col-auto"
+    >
+      <q-tooltip
+        anchor="center left"
+        self="center middle"
+        transition-show="scale"
+        transition-hide="scale"
+        :offset="[42,0]"
+      >
+        Changelog
+      </q-tooltip>
+    </q-btn>
   </q-footer>
 </template>

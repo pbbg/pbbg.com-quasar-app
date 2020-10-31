d9611f2863d960e76c221efb5ab084ee99ecd579
Brian Kennedy <bpkennedy@gmail.com>
2020-10-31 14:59:05 -0500

    Add Changelog view, route, and markdown quasar app extension

---
+++ src/pages/Changelog.vue
@@ -0,0 +1,21 @@
+<script>
+import markdown from 'assets/changelog.md'
+export default {
+  name: 'Changelog',
+  data() {
+    return {
+      markdown,
+    }
+  },
+}
+</script>
+
+<template>
+  <q-page
+    class="row justify-center items-baseline q-px-md q-pb-md"
+  >
+    <q-markdown>
+      {{ markdown }}
+    </q-markdown>
+  </q-page>
+</template>

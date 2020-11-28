0a0bed8db75753d52fc00af9def12b3eb4c85259
Brian Kennedy <bpkennedy@gmail.com>
2020-11-28 15:46:30 -0600

    Updating dependencies to latest versions

--- test/cypress/integration/home.spec.js
+++ test/cypress/integration/home.spec.js
@@ -11,7 +11,7 @@ describe('Home', function () {
 
   it('should retrieve the news feed when homepage loads', () => {
     cy.contains('News Feed').should('be.visible')
-    cy.getAriaLabel('News Feed Loading Icon').should('not.be.visible')
+    cy.getAriaLabel('News Feed Loading Icon').should('not.exist')
   })
 
   it('should display app version in footer and navigate to changelog', () => {
